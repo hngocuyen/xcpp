ffd1724ec219fcf94050daca6a3f39742f561c6f
Huynhngocuyen <139137335+hngocuyen@users.noreply.github.com>
2024-02-23 13:22:59 +0700

    Update xcpp.cpp

--- xcpp.cpp
+++ xcpp.cpp
@@ -2,28 +2,39 @@
 #include <cstdlib>
 #include <cstring>
 #include <string>
-int main(int iunqmvcl, char* argv[]) {
-    if (iunqmvcl != 2) {
-        std::cerr << "Usage: " << argv[0] << " <file.cpp>" << std::endl;
+
+int main(int argc, char* argv[]) {
+    if (argc != 2) {
+        std::cerr << "Usage: " << argv[0] << " <file.cpp or file.c>" << std::endl;
         return 1;
     }
-    std::string sc = argv[1];
-    if (sc.size() < 4 || sc.substr(sc.size() - 4) != ".cpp") {
-        std::cerr << "Error: Input file must have a .cpp extension" << std::endl;
+    std::string source_file = argv[1];
+    std::string extension = source_file.substr(source_file.find_last_of(".") + 1);
+    
+    if (extension != "cpp" && extension != "c") {
+        std::cerr << "Error: Input file must have a .cpp or .c extension" << std::endl;
         return 1;
     }
-    std::string pn = sc.substr(0, sc.size() - 4);
-    std::string cc = "g++ -o " + pn + " " + sc;
-    int ckq = std::system(cc.c_str());
-    if (ckq != 0) {
+    std::string program_name = source_file.substr(0, source_file.find_last_of("."));
+    std::string compiler_command;
+    if (extension == "cpp") {
+        compiler_command = "g++ -o " + program_name + " " + source_file;
+    } else if (extension == "c") {
+        compiler_command = "g++ -o " + program_name + " " + source_file;
+    }
+    int compilation_result = std::system(compiler_command.c_str());
+    if (compilation_result != 0) {
         std::cerr << "Compilation failed" << std::endl;
-        return ckq;
+        return compilation_result;
     }
-    std::string rc = "./" + pn;
-    int rkq = std::system(rc.c_str());
-    if (rkq != 0) {
+    
+    std::string run_command = "./" + program_name;
+    int execution_result = std::system(run_command.c_str());
+    
+    if (execution_result != 0) {
         std::cerr << "Error: Execution failed" << std::endl;
-        return rkq;
+        return execution_result;
     }
+    
     return 0;
 }
